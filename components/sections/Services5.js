04d4f6ee519a154184216a04fcbcf80575689d1e
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-09-25 15:52:53 +0100

    updated

--- components/sections/Services5.js
+++ components/sections/Services5.js
@@ -1,75 +1,120 @@
-import Link from "next/link"
+import Link from "next/link";
 const Services5 = () => {
-    return (
-        <>
-            <section className="services-section-five">
-                <div className="auto-container">
-                    {/* <div className="sec-title text-center">
+  return (
+    <>
+      <section className="services-section-five">
+        <div className="auto-container">
+          {/* <div className="sec-title text-center">
                         <span className="sub-title">Best Service</span>
                         <h2>Designing the future, preserving <br/> the past</h2>
                     </div> */}
 
-                    <div className="row">
-
-                        <div className="services-block-five col-lg-3 col-md-6 col-sm-12 wow fadeInUp card shadow-lg">
-                            <div className="inner-box">
-                                <div className="title-box">
-                                    <div className="icon-box"><span className="fa fa-legal"></span></div>
-                                    <h4 className="title"><Link href="page-services">Chamber Sitting</Link></h4>
-                                    <div className="text">You can view chamber sitting schedule.</div>
-                                </div>
-                                <div className="btn-box">
-                                    <Link href="page-services" className="read-more">View <i className="icon fa fa-arrow-right"></i></Link>
-                                    <h6 className="count">01</h6>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="services-block-five col-lg-3 col-md-6 col-sm-12 wow fadeInUp card shadow-lg" data-wow-delay="300ms">
-                            <div className="inner-box">
-                                <div className="title-box">
-                                    <div className="icon-box"><i className="icon flaticon-file"></i></div>
-                                    <h4 className="title"><Link href="page-services">Weekly Causelist</Link></h4>
-                                    <div className="text">Matters currently going on in the court.</div>
-                                </div>
-                                <div className="btn-box">
-                                    <Link href="page-services" className="read-more">View <i className="icon fa fa-arrow-right"></i></Link>
-                                    <h6 className="count">02</h6>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="services-block-five col-lg-3 col-md-6 col-sm-12 wow fadeInUp card shadow-lg" data-wow-delay="600ms">
-                            <div className="inner-box">
-                                <div className="title-box">
-                                    <div className="icon-box"><i className="icon flaticon-folder-outline"></i></div>
-                                    <h4 className="title"><Link href="page-services">Causelist Archive</Link></h4>
-                                    <div className="text">Past Matters decided on by the court.</div>
-                                </div>
-                                <div className="btn-box">
-                                    <Link href="page-services" className="read-more">View <i className="icon fa fa-arrow-right"></i></Link>
-                                    <h6 className="count">03</h6>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="services-block-five col-lg-3 col-md-6 col-sm-12 wow fadeInUp card shadow-lg" data-wow-delay="600ms">
-                            <div className="inner-box">
-                                <div className="title-box">
-                                    <div className="icon-box"><i className="icon flaticon-attachment"></i></div>
-                                    <h4 className="title"><Link href="page-services">Judgements</Link></h4>
-                                    <div className="text">Cases which judgement has been served.</div>
-                                </div>
-                                <div className="btn-box">
-                                    <Link href="page-services" className="read-more">View <i className="icon fa fa-arrow-right"></i></Link>
-                                    <h6 className="count">04</h6>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+          <div className="row">
+            <div className="services-block-five col-lg-3 col-md-6 col-sm-12 wow fadeInUp card shadow-lg">
+              <div className="inner-box">
+                <div className="title-box">
+                  <div className="icon-box">
+                    <span className="fa fa-legal"></span>
+                  </div>
+                  <h4 className="title">
+                    <Link href="page-services">Chamber Sitting</Link>
+                  </h4>
+                  <div className="text">
+                    You can view chamber sitting schedule.
+                  </div>
                 </div>
-            </section>
-        </>
-    );
+                <div className="btn-box">
+                  <Link
+                    href="page-services"
+                    className="read-more">
+                    View <i className="icon fa fa-arrow-right"></i>
+                  </Link>
+                  <h6 className="count">01</h6>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="services-block-five col-lg-3 col-md-6 col-sm-12 wow fadeInUp card shadow-lg"
+              data-wow-delay="300ms">
+              <div className="inner-box">
+                <div className="title-box">
+                  <div className="icon-box">
+                    <i className="icon flaticon-file"></i>
+                  </div>
+                  <h4 className="title">
+                    <Link href="page-services">Weekly Causelist</Link>
+                  </h4>
+                  <div className="text">
+                    Matters currently going on in the court.
+                  </div>
+                </div>
+                <div className="btn-box">
+                  <Link
+                    href="page-services"
+                    className="read-more">
+                    View <i className="icon fa fa-arrow-right"></i>
+                  </Link>
+                  <h6 className="count">02</h6>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="services-block-five col-lg-3 col-md-6 col-sm-12 wow fadeInUp card shadow-lg"
+              data-wow-delay="600ms">
+              <div className="inner-box">
+                <div className="title-box">
+                  <div className="icon-box">
+                    <i className="icon flaticon-folder-outline"></i>
+                  </div>
+                  <h4 className="title">
+                    <Link href="page-services">Causelist Archive</Link>
+                  </h4>
+                  <div className="text">
+                    Past Matters decided on by the court.
+                  </div>
+                </div>
+                <div className="btn-box">
+                  <Link
+                    href="page-services"
+                    className="read-more">
+                    View <i className="icon fa fa-arrow-right"></i>
+                  </Link>
+                  <h6 className="count">03</h6>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="services-block-five col-lg-3 col-md-6 col-sm-12 wow fadeInUp card shadow-lg"
+              data-wow-delay="600ms">
+              <div className="inner-box">
+                <div className="title-box">
+                  <div className="icon-box">
+                    <i className="icon flaticon-attachment"></i>
+                  </div>
+                  <h4 className="title">
+                    <Link href="page-services">Judgements</Link>
+                  </h4>
+                  <div className="text">
+                    Cases which judgement has been served.
+                  </div>
+                </div>
+                <div className="btn-box">
+                  <Link
+                    href="page-services"
+                    className="read-more">
+                    View <i className="icon fa fa-arrow-right"></i>
+                  </Link>
+                  <h6 className="count">04</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
-export default Services5
+export default Services5;
--- components/sections/management-staffs.js
+++ components/sections/management-staffs.js
@@ -1,5 +1,24 @@
+import React, { useState, useEffect } from "react";
+import endpoint from "../../utils/endpoint";
 import Link from "next/link";
 const ManagementStaffs = () => {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const res = await endpoint.get("/management-staff");
+      console.log('====================================');
+      console.log(res.data.data);
+      console.log('====================================');
+      setData(res.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
       <section
@@ -14,128 +33,38 @@ const ManagementStaffs = () => {
           </div>
 
           <div className="row">
-            <div className="team-block-seven col-lg-4 col-md-6 col-sm-12 wow ">
-              <div className="inner-box">
-                <div className="image-box">
-                  <figure className="image">
-                    <Link href="#">
-                      <img
-                        src="/images/resource/mgt/mgtsarki1.jpg"
-                        alt=""
-                      />
-                    </Link>
-                  </figure>
+            {data &&
+              data.map((staff, index) => (
+                <div
+                  className="team-block-seven col-lg-4 col-md-6 col-sm-12 wow "
+                  key={index}>
+                  <div className="inner-box">
+                    <div className="image-box">
+                      <figure className="image">
+                        <Link href={`/management/${staff.id}`}>
+                          <img
+                            src={
+                              process.env.NEXT_PUBLIC_UPLOAD_URL +
+                              "management/" +
+                              staff.picture
+                            }
+                            alt={staff.fullname}
+                          />
+                        </Link>
+                      </figure>
+                    </div>
+                    <div className="info-box">
+                      <h4 className="name">
+                        <Link href={`/management/${staff.id}`}>
+                          {staff.title + " "}
+                          {staff.fullname}
+                        </Link>
+                      </h4>
+                      <span className="designation">{staff.position}</span>
+                    </div>
+                  </div>
                 </div>
-                <div className="info-box">
-                  <h4 className="name">
-                    <Link href="#">MRS. Hajo Sarki Bello</Link>
-                  </h4>
-                  <span className="designation">Cheif Registrar</span>
-                </div>
-              </div>
-            </div>
-            <div className="team-block-seven col-lg-4 col-md-6 col-sm-12 wow ">
-              <div className="inner-box">
-                <div className="image-box">
-                  <figure className="image">
-                    <Link href="#">
-                      <img
-                        src="/images/resource/mgt/mgtjego2.jpg"
-                        alt=""
-                      />
-                    </Link>
-                  </figure>
-                </div>
-                <div className="info-box">
-                  <h4 className="name">
-                    <Link href="#">Mr. Reuben M. A. Jego</Link>
-                  </h4>
-                  <span className="designation">Deputy Cheif Registrar I</span>
-                </div>
-              </div>
-            </div>
-            <div className="team-block-seven col-lg-4 col-md-6 col-sm-12 wow ">
-              <div className="inner-box">
-                <div className="image-box">
-                  <figure className="image">
-                    <Link href="#">
-                      <img
-                        src="/images/resource/mgt/mgtbabasoro3.jpg"
-                        alt=""
-                      />
-                    </Link>
-                  </figure>
-                </div>
-                <div className="info-box">
-                  <h4 className="name">
-                    <Link href="#">Mr. Yusuf Abubakar Babasoro</Link>
-                  </h4>
-                  <span className="designation">Deputy Cheif Registrar II</span>
-                </div>
-              </div>
-            </div>
-            <div className="team-block-seven col-lg-4 col-md-6 col-sm-12 wow ">
-              <div className="inner-box">
-                <div className="image-box">
-                  <figure className="image">
-                    <Link href="#">
-                      <img
-                        src="/images/resource/mgt/mgtsani4.jpg"
-                        alt=""
-                      />
-                    </Link>
-                  </figure>
-                </div>
-                <div className="info-box">
-                  <h4 className="name">
-                    <Link href="#">Mr. Ibrahim Sani Mohammed, mni</Link>
-                  </h4>
-                  <span className="designation">Director Training</span>
-                </div>
-              </div>
-            </div>{" "}
-            <div className="team-block-seven col-lg-4 col-md-6 col-sm-12 wow ">
-              <div className="inner-box">
-                <div className="image-box">
-                  <figure className="image">
-                    <Link href="#">
-                      <img
-                        src="/images/resource/mgt/mgtakande5.jpg"
-                        alt=""
-                      />
-                    </Link>
-                  </figure>
-                </div>
-                <div className="info-box">
-                  <h4 className="name">
-                    <Link href="#">Dr. Festus Aweneri Akande</Link>
-                  </h4>
-                  <span className="designation">
-                    Director Press and Information
-                  </span>
-                </div>
-              </div>
-            </div>{" "}
-            <div className="team-block-seven col-lg-4 col-md-6 col-sm-12 wow ">
-              <div className="inner-box">
-                <div className="image-box">
-                  <figure className="image">
-                    <Link href="#">
-                      <img
-                        src="/images/resource/mgt/mgtuka6.jpg"
-                        alt=""
-                      />
-                    </Link>
-                  </figure>
-                </div>
-                <div className="info-box">
-                  <h4 className="name">
-                    <Link href="#"> Dr. Ochea Ochea Ukai</Link>
-                  </h4>
-                  <span className="designation">Acting Medical Director</span>
-                </div>
-              </div>
-            </div>
+              ))}
           </div>
         </div>
       </section>
