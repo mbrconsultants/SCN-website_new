f59a6cac5e20b09c045a7dbd70f6a0bcbd4bc47d
Uloko John Igbo <johnuloks@gmail.com>
2024-10-03 11:35:40 +0100

    added publication

---
+++ pages/publication-list.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from "@/components/layout/Layout";
+import PublicationList from '@/components/sections/innerpages/PublicationhList';
+
+const Publication = () => {
+  
+    return (
+      <>
+        <Layout>
+          <PublicationList />
+        </Layout>
+      </>
+    );
+}
+
+export default Publication;
