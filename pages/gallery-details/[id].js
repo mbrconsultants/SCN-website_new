3ce012b53b723a7c9d9e16298cec1749a682d360
Shedrack Ogwuche <shedrackogwuche5@gmail.com>
2024-10-07 17:06:27 +0100

    updated

---
+++ pages/gallery-details/[id].js
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "@/components/layout/Layout";
+import GalleryDetail from "@/components/sections/innerpages/Gallery";
+
+const GalleryDetails = () => {
+  return (
+    <>
+      <Layout>
+        {/* <PageTitle pageName="About Us" /> */}
+
+        <GalleryDetail />
+      </Layout>
+    </>
+  );
+};
+
+export default GalleryDetails;
