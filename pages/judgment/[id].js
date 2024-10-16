2075d07df8ff34853828a80db6888fd07af4b150
pitoff <peteroffodile@gmail.com>
2024-10-16 11:53:36 +0100

    updated judgments

---
+++ pages/judgment/[id].js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "@/components/layout/Layout";
+import SingleJudgement from "@/components/sections/SingleJudgement";
+
+const JusticeProfile = () => {
+  return (
+    <>
+      <Layout>
+        <SingleJudgement />
+      </Layout>
+    </>
+  );
+};
+
+export default JusticeProfile;
